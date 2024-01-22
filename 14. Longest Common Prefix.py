f4d8eb7bf70ebf255eef9d56fb70ec858c6085e3
Lautaro Cepero <110281392+M1lcstery@users.noreply.github.com>
2024-01-22 17:47:47 -0300

    Add files via upload

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
+        if not strs:
+            return ""
+        
+        strs.sort()
+        
+        first_str = strs[0]
+        last_str = strs[-1]
+        
+        common_prefix = []
+        
+        for i in range(len(first_str)):
+            if i < len(last_str) and first_str[i] == last_str[i]:
+                common_prefix.append(first_str[i])
+            else:
+                break
+        return "".join(common_prefix)
